a2c14b78548d279c83176d4585ecc3695f6c8e0b
Roberto Magno <magnobeto.rm@gmail.com>
2020-08-28 18:24:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -12,6 +14,9 @@ public class Program {
     public static void main(String[] args){
         
         Department obj = new Department(1,"Books");
-        System.out.println(obj);
+        
+        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
+        
+        System.out.println(seller);
     }
 }
